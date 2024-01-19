6bca16a7522a196c61735716418ed99cd64bcbae
Paweł Urycki <uryckip@gmail.com>
2024-01-19 11:43:51 +0100

    Poprawa kodu

--- program.c
+++ program.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 
-float a;
-float b;
-float h;
-
 float obliczObjetoscProstopadloscianu(float a, float b, float h) {
     return a * b * h;
 }
 
-
 void menuObjetosciProstopadloscianu() {
-    printf("Podaj d³ugosci prostopadloscianu\n");
+    float a;
+    float b;
+    float h;
+
+    printf("Podaj dlugosci prostopadloscianu\n");
     printf("a: ");
     scanf("%f", &a);
+
     printf("b: ");
     scanf("%f", &b);
+
     printf("h: ");
     scanf("%f", &h);
-    printf("ObjetoscProstopadloscianu wynosi %f", obliczObjetoscProstopadloscianu(a, b, h));
-}
 
+    printf("Objetosc prostopadloscianu wynosi %f", obliczObjetoscProstopadloscianu(a, b, h));
+}
 
 int main() {
     menuObjetosciProstopadloscianu();
-
     return 0;
 }
